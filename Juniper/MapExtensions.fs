da8a8dead239beb582d575beee5eb1422b0ff464
Caleb Helbling <Caleb Helbling>
2016-02-21 15:15:23 -0500

    tenv work

---
+++ Juniper/MapExtensions.fs
@@ -0,0 +1,3 @@
+﻿module MapExtensions
+
+let merge map1 map2 = Map.fold (fun acc key value -> Map.add key value acc) map1 map2
--- Juniper/TypeChecker.fs
+++ Juniper/TypeChecker.fs
@@ -23,13 +23,59 @@ let typesInModule (Module decs) =
                              | TypeAliasDec {name=name} -> name
                              | _ -> failwith "This should never happen") typeDecs
 
+let opensInModule (Module decs) =
+    let opens = List.filter (fun dec -> match dec with
+                                            | OpenDec _ -> true
+                                            | _ -> false) decs
+    List.concat (List.map (fun dec -> match dec with
+                             | OpenDec names -> names
+                             | _ -> failwith "This should never happen") opens)
+
 let qualifierWrap modName decName =
     {module_ = modName; name = decName}
 
-let typecheck (modlist : Module list) = 
-    let modNameToTypesInMod = List.fold (fun map modl ->
-                                            let modName = nameInModule modl
-                                            let wrap = List.map (qualifierWrap modName)
-                                            Map.add modName (wrap <| typesInModule modl) map)
-                                        Map.empty modlist
-    modNameToTypesInMod
+let typecheck (modlist : Module list) =
+    // Maps module qualifiers to type definitions
+    let modQualToTypeDef = (List.fold (fun map (Module decs) ->
+        let modName = nameInModule (Module decs)
+
+        let typeDecs = List.filter (fun dec -> match dec with
+                                                    | RecordDec _ -> true
+                                                    | UnionDec _  -> true
+                                                    | TypeAliasDec _ -> true
+                                                    | _ -> false) decs
+        List.fold (fun map2 dec ->
+            let decName = match dec with
+                                    | RecordDec {name=name} -> name
+                                    | UnionDec {name=name} -> name
+                                    | TypeAliasDec {name=name} -> name
+                                    | _ -> failwith "This should never happen"
+            let qual = qualifierWrap modName decName
+            Map.add qual dec map2) map typeDecs
+    ) Map.empty modlist)
+    
+    // Now build the type environments for every module
+    // maps names to module qualifiers
+    let tenvs0 = (List.map (fun (Module decs) ->
+        let modName = nameInModule (Module decs)
+        let names = typesInModule (Module decs)
+        List.fold (fun map2 name ->
+           Map.add name (qualifierWrap modName name) map2
+        ) Map.empty names
+    ) modlist)
+
+    let modNamesToTenvs = Map.ofList (List.zip (List.map nameInModule modlist) tenvs0)
+
+    let modNamesToAst = Map.ofList (List.zip (List.map nameInModule modlist) modlist)
+
+    // Now deal with open declarations
+    let tenvs1 = (Map.map (fun modName tenv0 ->
+        let allOpens = opensInModule (Map.find modName modNamesToAst)
+        List.fold (fun tenv1 nameToMerge ->
+            MapExtensions.merge (Map.find nameToMerge modNamesToTenvs) tenv1 
+        ) tenv0 allOpens
+    ) modNamesToTenvs)
+
+    printfn "%A" tenvs1
+
+    ()

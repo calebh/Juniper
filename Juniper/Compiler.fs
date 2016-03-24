084ece8c65039089773e618a80af01904ebf1151
Caleb Helbling <Caleb Helbling>
2016-03-24 18:32:25 -0400

    Switched the compiler to use string appending

--- Juniper/Compiler.fs
+++ Juniper/Compiler.fs
@@ -1,27 +1,31 @@
 ﻿module Compiler
 open Ast
 
-let compiledResult = new System.Text.StringBuilder()
-
 let mutable indentationLevel = 0
 let mutable isNewLine = true
 let indent () = indentationLevel <- indentationLevel + 1
 let unindent () = indentationLevel <- indentationLevel - 1
 
-let output (str : string) =
+let indentId () =
+    indent()
+    ""
+
+let unindentId () =
+    unindent()
+    ""
+
+let output (str : string) : string =
     if isNewLine then
         isNewLine <- false
-        for i in 0 .. indentationLevel - 1 do
-            compiledResult.Append("    ") |> ignore
+        (String.replicate indentationLevel "    ") + str
     else
-        ()
-    compiledResult.Append(str) |> ignore
+        str
 
 let newline () =
     isNewLine <- true
-    compiledResult.Append("\n") |> ignore
+    "\n"
 
-let rec compileType (ty : TyExpr) =
+let rec compileType (ty : TyExpr) : string =
     match ty with
         | BaseTy (_, _, bty) ->
             match bty with
@@ -36,69 +40,142 @@ let rec compileType (ty : TyExpr) =
                 | TyFloat -> output "float"
                 | TyBool -> output "bool"
                 | TyUnit -> output "unit"
+        | TyModuleQualifier {module_ = (_, _, module_); name=(_, _, name)} ->
+            output module_ +
+            output "::" +
+            output name
+        | TyName (_, _, name) ->
+            output name
+        | TyApply {tyConstructor=(_, _, tyConstructor); args=(_, _, args)} ->
+            compileType tyConstructor +
+            output "<"
 
-and compileLeftAssign (left : LeftAssign) : unit =
+and compileLeftAssign (left : LeftAssign) : string =
     match left with
         | VarMutation {varName=(_, _, varName)} ->
             output varName
         | ArrayMutation {array=(_, _, array); index=index} ->
-            output "("
-            compileLeftAssign array
-            output ")["
-            compile index
+            output "(" +
+            compileLeftAssign array +
+            output ")[" +
+            compile index +
             output "]"
         | RecordMutation {record=(_, _, record); fieldName=(_, _, fieldName)} ->
-            output "("
-            compileLeftAssign record
-            output ")."
+            output "(" +
+            compileLeftAssign record +
+            output ")." +
             output fieldName
 
-and compile ((_, ty, expr) : PosAdorn<Expr>) : unit =
+and compile ((_, ty, expr) : PosAdorn<Expr>) : string =
     match expr with
         | IfElseExp {condition=condition; trueBranch=trueBranch; falseBranch=falseBranch} ->
-            output "("
-            compile condition
-            output " ? "
-            newline()
-            indent()
-            compile trueBranch
-            unindent()
-            newline()
-            output ":"
-            newline()
-            indent()
-            compile falseBranch
-            output ")"
-            unindent()
+            output "(" +
+            compile condition +
+            output " ? " +
+            newline() +
+            indentId() +
+            compile trueBranch +
+            unindentId() +
+            newline() +
+            output ":" +
+            newline() +
+            indentId() +
+            compile falseBranch +
+            output ")" +
+            unindentId()
         | SequenceExp (_, Some tySeq, sequence) ->
-            output "(([&]() -> "
-            compileType tySeq
-            output " {"
-            newline()
-            indent()
             let len = List.length sequence
-            sequence |> List.iteri
-                (fun i seqElement ->
-                    if i = len - 1 then
-                        ()
-                    else
-                        compile seqElement
-                        output ";"
-                        newline())
-            output "return "
-            compile (List.last sequence)
-            output ";"
-            newline()
-            unindent()
+            output "(([&]() -> " +
+            compileType tySeq +
+            output " {" +
+            newline() +
+            indentId() +
+            ((List.mapi (fun i seqElement ->
+                (if i = len - 1 then
+                    output "return "
+                else
+                    output "") +
+                compile seqElement +
+                output ";" +
+                newline()
+            ) sequence) |> String.concat "") +
+            unindentId() +
             output "})())"
         | AssignExp {left=(_, _, left); right=right; ref=(_, _, ref)} ->
-            output "("
-            if ref then
+            output "(" +
+            (if ref then
                 output "*"
-            compileLeftAssign left
-            output " = "
-            compile right
+            else
+                "") +
+            compileLeftAssign left +
+            output " = " +
+            compile right +
             output ")"
 
+and compileTemplate (template : Template) : string = 
+    let tyVars = template.tyVars |> unwrap |> List.map (unwrap >> (+) "typename ")
+    let capVars = template.capVars |> unwrap |> List.map (unwrap >> (+) "int ")
+    output "template<" +
+    (List.append tyVars capVars |> String.concat ", " |> output) +
+    output ">"
+
+and compileCap (cap : CapacityExpr) : string =
+    ""
+
+and compileTemplateApply (templateApp : TemplateApply) : string =
+    output "<" +
+    ((List.append
+        (templateApp.tyExprs |> unwrap |> List.map (unwrap >> compileType))
+        (templateApp.capExprs |> unwrap |> List.map (unwrap >> compileCap))) |> String.concat ", ") +
+    output ">"
+
 and compileDec (dec : Declaration) =
-    
+    match dec with
+        | FunctionDec {name=(_, _, name); template=maybeTemplate; clause=(_, _, clause)} ->
+            (match maybeTemplate with
+                | Some (_, _, template) ->
+                    compileTemplate template +
+                    newline()
+                | None ->
+                    output "") +
+            (clause.returnTy |> unwrap |> compileType) +
+            output " " +
+            output name +
+            output "(" +
+            ((clause.arguments |> unwrap |> List.map (fun (ty, name) ->
+                                                         (ty |> unwrap |> compileType) +
+                                                         output " " +
+                                                         (name |> unwrap |> output))) |> String.concat ", ") +
+            output ") {" +
+            newline() +
+            indentId() +
+            compile clause.body +
+            unindentId() +
+            newline() +
+            output "}"
+        | RecordDec {name=(_, _, name); fields=fields; template=maybeTemplate} ->
+            (match maybeTemplate with
+                | Some (_, _, template) ->
+                    compileTemplate template +
+                    newline()
+                | None ->
+                    output "") +
+            output "struct " +
+            output name +
+            output " {" +
+            newline() +
+            indentId() +
+            ((fields |> unwrap |> List.map (fun (fieldTy, fieldName) ->
+                                                compileType fieldTy +
+                                                output " " +
+                                                output fieldName +
+                                                output ";")) |> (String.concat "")) +
+            newline() +
+            unindentId() +
+            output "};"
+        | LetDec {varName=(_, _, varName); typ=(_, _, typ); right=right} ->
+            compileType typ +
+            output " " +
+            output varName +
+            output " = " +
+            compile right

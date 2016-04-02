b9d37b2435fa20d22e159d68c0324924a8656dba
Caleb Helbling <Caleb Helbling>
2016-04-02 01:41:02 -0400

    Added math c header file to Juniper programs

--- Juniper/Compiler.fs
+++ Juniper/Compiler.fs
@@ -486,7 +486,8 @@ and compileDec (dec : Declaration) : string =
 and compileProgram (modList : Module list) : string =
     output "#include <inttypes.h>" + newline() +
     output "#include \"juniper.hpp\"" + newline() +
-    output "#include <stdbool.h>" + newline() + newline() +
+    output "#include <stdbool.h>" + newline() +
+    output "#include <math.h>" + newline() + newline() +
     (modList |> List.map (fun (Module module_)->
         let moduleName = Module module_ |> Module.nameInModule |> unwrap
         output "namespace " + moduleName + output " {" + newline() +
